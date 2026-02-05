b9737557a6d4ab9733930aadb0be4b92b90fefd3
sonicisa456 <isaiasrv2019@outlook.com>
2026-02-05 13:22:08 -0600

    creamos un usuario y un admin adicional

--- ACTIVIDAD_3/users.js
+++ ACTIVIDAD_3/users.js
@@ -1,3 +1,6 @@
 const users = [
-  { username: "infinite", password: "infinite123", role: "admin" }
+  { username: "infinite", password: "infinite123", role: "admin" },
+  { username: "mordecai", password: "verdaderomordecai456", role: "admin"},
+
+  { username: "rigby", password: "intensorigby", role: "user" }
 ];
